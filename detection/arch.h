22b65c59dcd4e213cec42925127b40e96b1638fe
Caleb Zulawski <czulawski@lgsinnovations.com>
2021-01-14 12:49:21 -0500

    Add specifying toolchain to context

---
+++ detection/arch.h
@@ -0,0 +1,11 @@
+#if defined(__arm__)
+    "arm"
+#elif defined(__aarch64__)
+    "aarch64"
+#elif defined(__i386__)
+    "i386"
+#elif defined(__x86_64__)
+    "x86-64"
+#else
+    "unknown"
+#endif
